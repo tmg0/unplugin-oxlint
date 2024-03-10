99f8b8735e02d02a487c2162eb52e7595d8106f6
tmg0 <zekun.jin@foxmail.com>
2024-03-10 09:10:57 +0800

    fix: pnpm global install

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,4 +1,4 @@
 import { addDevDependency } from 'nypm'
 import { peerDependencies } from '../package.json'
 
-addDevDependency(Object.keys(peerDependencies))
+addDevDependency(Object.keys(peerDependencies), { global: true })
