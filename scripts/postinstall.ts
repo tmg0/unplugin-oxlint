018cfa3af9c6726277222d05e14bd4dccf49471e
tmg0 <zekun.jin@foxmail.com>
2024-03-10 11:48:27 +0800

    fix: postinstall script

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,4 +1,4 @@
 import { addDevDependency } from 'nypm'
 import { peerDependencies } from '../package.json'
 
-addDevDependency(Object.keys(peerDependencies), { global: true })
+addDevDependency(Object.keys(peerDependencies), { workspace: true })
