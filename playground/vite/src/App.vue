0080f854332ee1bc88692141427d69e050301ccb
tmg0 <zekun.jin@foxmail.com>
2024-03-13 11:39:54 +0800

    feat: setup vite playground

---
+++ playground/vite/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import {ref } from "vue" 
+
+const msg = ref('Vite + Vue')
+
+
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo"
+      alt="Vite logo" />
+    </a>
+  </div>
+  <div>{{msg }}
+    </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
