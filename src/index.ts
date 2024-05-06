c8c45de51e6e301e1ab52cbd4bfd3268828dcc56
tmg0 <zekun.jin@foxmail.com>
2024-05-06 11:23:16 +0800

    feat: parse conf by c12

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import process from 'node:process'
 import mri from 'mri'
+import { loadConfig } from 'c12'
 import type { OxlintOptions } from './core/types'
 import { unplugin } from './core/unplugin'
 import { createOxlint } from './core/context'
-
-export type LintOptions = Omit<OxlintOptions, 'watch'>
+import { DEFAULT_OXLINT_OPTIONS } from './core/options'
 
 export async function runMain() {
   const argv = mri(process.argv.slice(2), { boolean: ['watch'] })
-  createOxlint({ ...argv, includes: argv._ }).setup()
+  const { config } = await loadConfig({ name: 'unox', defaults: DEFAULT_OXLINT_OPTIONS })
+  createOxlint({ ...config, includes: argv._ }).setup()
+}
+
+export function defineConfig(options: Partial<OxlintOptions> = {}) {
+  return options
 }
 
 export default unplugin
