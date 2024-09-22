0d429f5d369bde50ff06ab74cfb261991cc8a7c1
tmg0 <zekun.jin@foxmail.com>
2024-09-22 19:15:32 +0800

    chore: upgrade deps

--- playground/tsup/tsup.config.ts
+++ playground/tsup/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 import Oxlint from '../../src/esbuild'
 
 export default defineConfig(({ watch }) => ({
-  entry: ['src/index.ts'],
+  entry: ['./src/index.ts'],
   clean: true,
   esbuildPlugins: [
     Oxlint({
